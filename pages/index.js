d82fdd73d831ec92377e14476467e98d18398605
resjudicoda <adam.j.reis@gmail.com>
2022-11-28 20:29:32 -0800

    minor ui change

--- pages/index.js
+++ pages/index.js
@@ -62,7 +62,7 @@ const Home = () => {
               <div className="output">
                 <div className="output-header-container">
                   <div className="output-header">
-                    <h3>Output</h3>
+                    <h3>Your tweet</h3>
                   </div>
                 </div>
                 <div className="output-content">
